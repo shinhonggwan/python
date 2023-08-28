2aa69018dec63278219fa73b866e14965538bae2
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2023-08-28 15:43:21 +0900

    Add files via upload

---
+++ squared_sum.py
@@ -0,0 +1,13 @@
+# Complete the square sum function so that 
+#it squares each number passed into it and then 
+# sums the results together.
+
+# For example, for [1, 2, 2] it should return 9 
+# because 12+22+22=91^2 + 2^2 + 2^2 = 912+22+22=9.
+
+def square_sum(numbers):
+    #your code here
+    answer = 0
+    for i in numbers:
+        answer += i*i
+    return answer
