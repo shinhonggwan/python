f0a3dd22c9aa608892d027240df41311ffe48b84
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2023-08-31 15:34:16 +0900

    Add files via upload

---
+++ no_space.py
@@ -0,0 +1,14 @@
+# Write a function that removes the spaces from the string, then return the resultant string.
+# Examples:
+# "8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+# "8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+# "8aaaaa dddd r     " -> "8aaaaaddddr"
+
+def no_space(x):
+    #your code here
+    new_string = ""
+    if " " in x:
+        new_string = x.replace(' ', '')
+    else:
+        return x
+    return new_string
