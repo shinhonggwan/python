fece5920bd88c0f5493c9e24b5b90ef53805b2ee
Honggwan Shin <74944060+shinhonggwan@users.noreply.github.com>
2023-09-04 16:42:26 +0900

    Create double.py

---
+++ double.py
@@ -0,0 +1,9 @@
+# Given an array of integers, return a new array with each value doubled.
+# For example:
+# [1, 2, 3] --> [2, 4, 6]
+
+def maps(a):
+    new = []
+    for i in a:
+        new.append(i*2)
+    return new
